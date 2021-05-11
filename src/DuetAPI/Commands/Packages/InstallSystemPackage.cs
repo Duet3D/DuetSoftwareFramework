978660f304c1ba18771771529a21b8585f99870f
Christian Hammacher <bmasterc@gmail.com>
2021-05-11 19:41:45 +0200

    Added missing files

---
+++ src/DuetAPI/Commands/Packages/InstallSystemPackage.cs
@@ -0,0 +1,17 @@
+﻿using DuetAPI.Utility;
+using System;
+
+namespace DuetAPI.Commands
+{
+    /// <summary>
+    /// Install or upgrade a system package
+    /// </summary>
+    [RequiredPermissions(SbcPermissions.SuperUser)]
+    public class InstallSystemPackage : Command
+    {
+        /// <summary>
+        /// Absolute file path to the package file
+        /// </summary>
+        public string PackageFile { get; set; }
+    }
+}
---
+++ src/DuetAPI/Commands/Packages/UninstallSystemPackage.cs
@@ -0,0 +1,16 @@
+﻿using DuetAPI.Utility;
+
+namespace DuetAPI.Commands
+{
+    /// <summary>
+    /// Uninstall a system package
+    /// </summary>
+    [RequiredPermissions(SbcPermissions.SuperUser)]
+    public class UninstallSystemPackage : Command
+    {
+        /// <summary>
+        /// Identifier of the package
+        /// </summary>
+        public string Package { get; set; }
+    }
+}
