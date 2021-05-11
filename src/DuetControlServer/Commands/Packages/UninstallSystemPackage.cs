43b99fbbbb82811891a080ab3e9a52a46ffbc4ff
Christian Hammacher <bmasterc@gmail.com>
2021-05-11 19:44:40 +0200

    Code cleanup and added two more missing files

---
+++ src/DuetControlServer/Commands/Packages/UninstallSystemPackage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DuetControlServer.Commands
+{
+    /// <summary>
+    /// Implementation of the <see cref="DuetAPI.Commands.UninstallSystemPackage"/> command
+    /// </summary>
+    public sealed class UninstallSystemPackage : DuetAPI.Commands.UninstallSystemPackage
+    {
+        /// <summary>
+        /// Uninstall a system package
+        /// </summary>
+        /// <returns>Asynchronous task</returns>
+        /// <exception cref="ArgumentException">Package could not be uninstalled</exception>
+        public override async Task Execute()
+        {
+            if (!Settings.RootPluginSupport)
+            {
+                throw new NotSupportedException("Root plugin support has been disabled");
+            }
+
+            // Forward this command to the plugin services
+            await IPC.Processors.PluginService.PerformCommand(this, true);
+        }
+    }
+}
