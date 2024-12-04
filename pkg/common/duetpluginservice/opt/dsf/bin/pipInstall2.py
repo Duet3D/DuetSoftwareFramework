d9c9dc3d1422188b54129e85b94fdcc4e7de1c9a
AndyEveritt <38423143+AndyEveritt@users.noreply.github.com>
2024-12-04 13:58:30 +0000

    Update python package install script to handle a more comprehensive range of packages

--- pkg/common/duetpluginservice/opt/dsf/bin/pipInstall2.py
+++ pkg/common/duetpluginservice/opt/dsf/bin/pipInstall2.py
@@ -52,6 +52,9 @@ import sysconfig
 import argparse
 import json
 
+from enum import Enum, auto
+from typing import Optional
+
 # CONSTANTS
 VENV_FOLDER = 'venv'
 MANIFEST_KEY = 'sbcPythonDependencies'
@@ -63,6 +66,105 @@ if os.name == 'nt': # Windows
 else:
     BIN_DIR = 'bin'
     PYTHON_VERSION = 'python'
+
+
+class ExitCodes(Enum):
+    NO_MANIFEST_PROVIDED = 1
+    MANIFEST_DOES_NOT_EXIST = 2
+    NO_PLUGIN_PROVIDED = 3
+    PLUGIN_DOES_NOT_EXIST = 4
+    UNSUPPORTED_CONDITIONAL = 5
+    PROBLEM_CREATING_VENV = 6
+    MANIFEST_ERROR = 7
+    PIP_LIST_ERROR = 8
+    UNEXPECTED_ERROR = 9
+    FAILED_TO_INSTALL_MODULE = 10
+    INVALID_DEPENDENCY = 11
+
+
+class Dependency:
+    regex = r'(^([\w\-_]+)((==|~=|>=|<=|>|<)((\d+!)?(\d)+(\.\d+)*((a|b|rc)\d+)?(\.post\d+)?(\.dev\d+)?))?$)|(^git\+.*$)'
+
+    class RegexGroups(Enum):
+        PYPI = 0
+        PACKAGE_NAME = 1
+        VERSION_WITH_COMPARATOR = 2
+        COMPARATOR = 3
+        VERSION = 4
+        VERSION_EPOCH = 5
+        VERSION_MAJOR = 6
+        VERSION_MINOR = 7
+        VERSION_PRE = 8
+        VERSION_PRE_TYPE = 9
+        VERSION_POST = 10
+        VERSION_DEV = 11
+        GIT = 12
+
+    class DepTypes(Enum):
+        PYPI = auto()
+        GIT = auto()
+        NONE = auto()
+
+    def __init__(self) -> None:
+        self.package: str = None
+        self.version: str = None
+        self.comparator: str = None
+        self.type = self.DepTypes.NONE
+
+    @property
+    def uri(self):
+        package = "" if self.package is None else self.package
+        comparator = "" if self.comparator is None else self.comparator
+        version = "" if self.version is None else self.version
+        return package + comparator + version
+
+    @classmethod
+    def parse(cls, text: str) -> Optional['Dependency']:
+        """Convert text from plugin.json sbcPythonDependencies into object
+
+        Args:
+            text (str): line from sbcPythonDependencies
+        """
+        result = re.findall(cls.regex, text)
+
+        if len(result) == 0:
+            logger.error(f"Could not find package name in \"{text}\"")
+            return None
+
+        result = list(result[0])  # Convert tuple to list
+
+        dep = Dependency()
+
+        if result[cls.RegexGroups.PYPI] != '':
+            # Standard PyPi dependency
+            if result[cls.RegexGroups.COMPARATOR] == '~=':
+                result[cls.RegexGroups.COMPARATOR] = '>='
+
+            dep.type = cls.DepTypes.PYPI
+
+            # Convert to canonical name format
+            name = str(result[cls.RegexGroups.PACKAGE_NAME])
+            name = name.lower()
+            name = name.replace('-', '_')
+            name = name.replace('.', '_')
+            dep.package = name
+
+            dep.comparator = str(result[cls.RegexGroups.COMPARATOR])
+            dep.version = str(result[cls.RegexGroups.VERSION])
+
+        elif result[cls.RegexGroups.GIT] != '':
+            # Git dependency
+            dep.type = cls.DepTypes.GIT
+            dep.package = str(result[cls.RegexGroups.GIT])
+            dep.comparator = None
+            dep.version = None
+        else:
+            # Should be impossible to get here
+            logger.error(f"Could not find package name in \"{text}\"")
+            return None
+
+        return dep
+
 
 
 def createLogger(logname):   ##### Create a custom logger so messages go to journalctl#####
@@ -92,38 +194,33 @@ def validateParams():
 
     if mFile is None:
         logger.info('Exiting: No manifest file (-m) was provided')
-        sys.exit(1)
+        sys.exit(ExitCodes.NO_MANIFEST_PROVIDED)
     elif not os.path.isfile(mFile):
         logger.info('Exiting: Manifest file ' +mFile + ' does not exist')
-        sys.exit(1)
+        sys.exit(ExitCodes.MANIFEST_DOES_NOT_EXIST)
 
     if pPath is None:
         logger.info('Exiting: No plugin path (-p) was provided')
-        sys.exit(1)
+        sys.exit(ExitCodes.NO_PLUGIN_PROVIDED)
     elif not os.path.isdir(pPath):    
-        logger.info('Exiting: Manifest file ' +mFile + 'does not exist')
-        sys.exit(1)
+        logger.info('Exiting: Plugin file ' + mFile + 'does not exist')
+        sys.exit(ExitCodes.PLUGIN_DOES_NOT_EXIST)
         
     return mFile, pPath
 
-def parseVersion(request):
+
+def parseVersion(request) -> Dependency:
 # Get the module name any conditional and version
     if ',' in request:
         logger.info('Unsupported Conditional in: ' + str(request))
-        sys.exit(1)
+        sys.exit(ExitCodes.UNSUPPORTED_CONDITIONAL)
 
-    result = []
-    conditionals = '|'.join(['==', '~=', '>=', '<=', '>', '<'])
-    regex = '(.+)(' + conditionals + ')(.+)'
-    result = re.findall(regex,request)
+    dep = Dependency.parse(request)
 
-    if len(result) == 0: # No conditional
-        result = [(request, '', '')]
+    if dep is None:
+        sys.exit(ExitCodes.INVALID_DEPENDENCY)
 
-    result = list(result[0]) # Convert tuple to list
-    if result[1] == '~=':
-        result [1] = '>='
-    return result[0], result[1], result[2]
+    return dep
 
 def runsubprocess(cmd):
     try:
@@ -153,7 +250,7 @@ def createPythonEnv(envPath):
     if result != '':
         logger.info ('Problem creating Virtual Environment')
         logger.info(result)
-        sys.exit(1)
+        sys.exit(ExitCodes.PROBLEM_CREATING_VENV)
     return
 
 def getModuleList(mFile):
@@ -163,29 +260,29 @@ def getModuleList(mFile):
         except ValueError as e:
             logger.info(mFile + ' is not a properly formatted json file')
             logger.info(str(e))
-            exit(1)
+            sys.exit(ExitCodes.MANIFEST_ERROR)
 
     mList = config[MANIFEST_KEY]
 
     return mList
 
 def getInstalledVersion(m,envPath ):
+    if (m in sys.builtin_module_names):
+        return 'Built-In'
+
     try:
         globals()[m] = __import__(m)  #Will likely not work if alternate python versions allowed in future
         result = globals()[m].__version__
         return result
 
-    except AttributeError: # No version number
-        return 'Built-In'
-
-    except ImportError: #  Check to see if pip thinks its installed
+    except AttributeError or ImportError:  # Check to see if pip thinks its installed
         pythonFile = os.path.normpath(os.path.join(envPath,BIN_DIR,PYTHON_VERSION))
         cmd = pythonFile + ' -m ' + PIP + ' list'
 
         request = runsubprocess(cmd)
         if request is False:
             logger.info('Aborting: Failed to get pip list')
-            sys.exit(1)
+            sys.exit(ExitCodes.PIP_LIST_ERROR)
         # Normalise to lower case and underscore
         request = request.lower()
         request = request.replace('-','_')
@@ -202,12 +299,9 @@ def getInstalledVersion(m,envPath ):
             return 'None'
 
 
-def installModule(mRequest, mVersion, envPath):
-    upgrade = ''
-    if mVersion == '':
-        upgrade = ' --upgrade '
+def installModule(dep: Dependency, envPath: str):
     pythonFile = os.path.normpath(os.path.join(envPath,BIN_DIR,PYTHON_VERSION))
-    cmd = pythonFile + ' -m ' + PIP + ' install --no-cache-dir ' + upgrade +  '"' + mRequest + '"' 
+    cmd = pythonFile + ' -m ' + PIP + ' install --no-cache-dir --upgrade ' + '"' + dep.uri + '"'
 
     result = runsubprocess(cmd)
     if result == False:  # module could not be installed
@@ -220,23 +314,18 @@ def installModules(mList,envPath):
     fList = [] # Fail
     for requestedVersion in mList:
         # Get the elements of the requested module - parseVersion may modify
-        mName, mCompare, mVersion = parseVersion(requestedVersion)
-        # Change to canonical name
-        mName = mName.lower()
-        mName = mName.replace('-','_')
-        mName = mName.replace('.','_')
-        mRequested = mName+mCompare+mVersion
+        dep = parseVersion(requestedVersion)
 
-        logger.info('Checking for python module: ' + mRequested)
+        logger.info('Checking for python module: ' + dep.uri)
 
         # Check to see what is installed
-        installedVersion = getInstalledVersion(mName, envPath)
+        installedVersion = getInstalledVersion(dep.uri, envPath)
 
         #  Determine next action
         if installedVersion == 'Built-In': # Rule 1
             resultCode = 2
         else:
-            installOk = installModule(mRequested, mVersion, envPath)
+            installOk = installModule(dep, envPath)
             if installOk:
                 resultCode = 1
             elif installedVersion not in ['Built_in','None']:
@@ -246,18 +335,19 @@ def installModules(mList,envPath):
 
         # Exit the program with appropriate log entries
         if resultCode == 0:
-            sList.append('Module "' + mRequested + '" is already installed')
+            sList.append('Module "' + dep.uri + '" is already installed')
         elif resultCode == 1:
-            sList.append('Module "' + mName + '" was installed or updated to version ' +  getInstalledVersion(mName, envPath))
+            sList.append('Module "' + dep.package + '" was installed or updated to version ' +
+                         getInstalledVersion(dep.package, envPath))
         elif resultCode == 2:
-            sList.append('Module "' + mName + '" is a built-in.')
+            sList.append('Module "' + dep.package + '" is a built-in.')
         elif resultCode == 3:
-            fList.append('Module "' + mRequested + '"')
+            fList.append('Module "' + dep.uri + '"')
         elif resultCode == 4:
-            sList.append('Module "' + mName + '" was not updated from version ' + installedVersion)
+            sList.append('Module "' + dep.package + '" was not updated from version ' + installedVersion)
         else:
             logger.info('An unexpected error occured')
-            sys.exit(1)
+            sys.exit(ExitCodes.UNEXPECTED_ERROR)
 
     return sList, fList
 
@@ -295,7 +385,7 @@ def main(progName):
         for entry in failList:
             logger.info(entry)
         logger.info('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-        sys.exit(1)
+        sys.exit(ExitCodes.FAILED_TO_INSTALL_MODULE)
 
     logger.info('---------------------------------------')
     logger.info('All modules were successfully installed')
