c80eddfd97dae1f3dcb265150284dfa18b9e8627
Christian Hammacher <chr.hammacher@gmail.com>
2025-07-14 11:19:23 +0200

    Added more details to SPI disconnect error message

--- src/DuetControlServer/SPI/DataTransfer.cs
+++ src/DuetControlServer/SPI/DataTransfer.cs
@@ -1329,12 +1329,12 @@ namespace DuetControlServer.SPI
                         throw new OperationCanceledException("Program termination");
                     }
 
-                    if (stopwatch.ElapsedMilliseconds > timeout + 500)
+                    if (stopwatch.ElapsedMilliseconds > timeout + 125)
                     {
-                        // In case this application does not seem to get enough CPU time, log a different message
+                        // In case the CTS is triggered very late, this application may not have gotten enough CPU time. Log this
                         Logger.LogOutput(MessageType.Warning, "Did not get enough CPU time during SPI transfer, your SBC may be overloaded");
                     }
-                    throw new OperationCanceledException("Timeout while waiting for transfer ready pin");
+                    throw new OperationCanceledException($"{(inTransfer ? "Transfer" : "Connection")} timeout while waiting for TfrRdy pin");
                 }
 
                 // Keep track of the maximum wait times
