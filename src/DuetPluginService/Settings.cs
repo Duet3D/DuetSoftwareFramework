44f201b0ae4fe58122305d762962f9003c1b3e8f
Christian Hammacher <chr.hammacher@gmail.com>
2025-08-18 17:59:26 +0200

    Resolved warning

--- src/DuetPluginService/Settings.cs
+++ src/DuetPluginService/Settings.cs
@@ -304,7 +304,7 @@ namespace DuetPluginService
                         }
                         else if (property.PropertyType == typeof(LogLevel))
                         {
-                            property.SetValue(null, LogLevel.FromString(reader.GetString()));
+                            property.SetValue(null, LogLevel.FromString(reader.GetString()!));
                         }
                         else
                         {
