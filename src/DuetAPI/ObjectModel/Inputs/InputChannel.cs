a549711503bf28d4b124e5cf4e5c059dd20157b6
Christian Hammacher <chr.hammacher@gmail.com>
2025-11-06 11:41:20 +0100

    Updated inputs[].feedrate documentation

--- src/DuetAPI/ObjectModel/Inputs/InputChannel.cs
+++ src/DuetAPI/ObjectModel/Inputs/InputChannel.cs
@@ -60,7 +60,7 @@
         private bool _drivesRelative = true;
 
         /// <summary>
-        /// Current feedrate in mm/s
+        /// Current feedrate as passed by the last G0/G1 F... command
         /// </summary>
         public float FeedRate
         {
